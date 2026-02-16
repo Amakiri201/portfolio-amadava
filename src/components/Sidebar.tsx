2640727d4a47fdfd38ed38be2a0c0eb2eb8720d9
Amakiri201 <davidamakiri201@gmail.com>
2026-02-16 18:56:41 +0100

    refactor: Adjust sidebar horizontal position and update displayed role text.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Sidebar: React.FC = () => {
     return (
         <>
-            <div className="fixed left-6 top-1/2 -translate-y-1/2 z-40 hidden lg:block">
+            <div className="fixed left-0 top-1/2 -translate-y-1/2 z-40 hidden lg:block">
                 <div className="rotate-[-90deg] origin-center whitespace-nowrap text-[10px] font-medium text-brand-secondary tracking-widest uppercase">
-                    Product designer
+                    Frontend Developer
                 </div>
             </div>
             <div className="fixed left-6 bottom-16 z-40 hidden lg:block">
